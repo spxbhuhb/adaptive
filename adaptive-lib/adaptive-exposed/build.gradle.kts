b3491ea62fde9f7ced6062ccd915cac1e113ebe1
Tóth István Zoltán <tiz@simplexion.hu>
2024-05-02 12:05:34 +0200

    2024.05.02

--- adaptive-lib/adaptive-exposed/build.gradle.kts
+++ adaptive-lib/adaptive-exposed/build.gradle.kts
@@ -3,7 +3,7 @@
  */
 plugins {
     kotlin("multiplatform") version "1.9.10"
-    id("hu.simplexion.adaptive") version "2024.04.19"
+    id("hu.simplexion.adaptive") version "2024.05.02"
     java
     signing
     `maven-publish`
