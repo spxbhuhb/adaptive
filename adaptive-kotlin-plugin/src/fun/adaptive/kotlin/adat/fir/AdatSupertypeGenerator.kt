ee9628121875baf30f87ce2729ef0ede4b216470
Tóth István Zoltán <tiz@simplexion.hu>
2024-08-14 12:51:27 +0200

    plugin bugfix - adat supertype generation
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- adaptive-kotlin-plugin/src/fun/adaptive/kotlin/adat/fir/AdatSupertypeGenerator.kt
+++ adaptive-kotlin-plugin/src/fun/adaptive/kotlin/adat/fir/AdatSupertypeGenerator.kt
@@ -10,22 +10,31 @@ import org.jetbrains.kotlin.fir.extensions.FirDeclarationPredicateRegistrar
 import org.jetbrains.kotlin.fir.extensions.FirSupertypeGenerationExtension
 import org.jetbrains.kotlin.fir.extensions.predicate.LookupPredicate
 import org.jetbrains.kotlin.fir.extensions.predicateBasedProvider
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 import org.jetbrains.kotlin.fir.types.builder.buildResolvedTypeRef
 import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.fir.types.constructClassLikeType
+import org.jetbrains.kotlin.name.ClassId
 
 class AdatSupertypeGenerator(session: FirSession) : FirSupertypeGenerationExtension(session) {
 
     companion object {
-        val companions = mutableMapOf<FirClassSymbol<*>, FirClassLikeDeclaration>()
+        val companions = mutableSetOf<ClassId>()
     }
 
     val ADAT_PREDICATE = LookupPredicate.create { annotated(FqNames.ADAT_ANNOTATION) }
 
     override fun FirDeclarationPredicateRegistrar.registerPredicates() {
         register(ADAT_PREDICATE)
+    }
+
+    override fun needTransformSupertypes(declaration: FirClassLikeDeclaration): Boolean {
+        val matches = session.predicateBasedProvider.matches(ADAT_PREDICATE, declaration.symbol)
+        if (matches && declaration is FirRegularClass && declaration.companionObjectSymbol != null) {
+            companions += declaration.companionObjectSymbol !!.classId
+        }
+        val companion = declaration.symbol.classId in companions
+        return (matches || companion)
     }
 
     override fun computeAdditionalSupertypes(
@@ -35,12 +44,11 @@ class AdatSupertypeGenerator(session: FirSession) : FirSupertypeGenerationExtens
     ): List<FirResolvedTypeRef> {
 
         val symbol = classLikeDeclaration.symbol
-        val companion = companions[symbol]
 
-        if (companion != null) {
+        if (symbol.classId in companions) {
             if (resolvedSupertypes.any { it.type.classId == ClassIds.ADAT_COMPANION }) return emptyList()
 
-            val adatClassType = companion.symbol.classId.constructClassLikeType(emptyArray(), false)
+            val adatClassType = symbol.classId.constructClassLikeType(emptyArray(), false)
 
             return listOf(
                 buildResolvedTypeRef {
@@ -61,15 +69,6 @@ class AdatSupertypeGenerator(session: FirSession) : FirSupertypeGenerationExtens
                 }
             )
         }
-    }
-
-    override fun needTransformSupertypes(declaration: FirClassLikeDeclaration): Boolean {
-        val matches = session.predicateBasedProvider.matches(ADAT_PREDICATE, declaration.symbol)
-        if (matches && declaration is FirRegularClass && declaration.companionObjectSymbol != null) {
-            companions[declaration.companionObjectSymbol !!] = declaration
-        }
-        val companion = declaration.symbol in companions
-        return (matches || companion)
     }
 
 }
--- adaptive-kotlin-plugin/src/fun/adaptive/kotlin/foundation/ir/util/adat.kt
+++ adaptive-kotlin-plugin/src/fun/adaptive/kotlin/foundation/ir/util/adat.kt
@@ -3,13 +3,14 @@ package `fun`.adaptive.kotlin.foundation.ir.util
 import `fun`.adaptive.kotlin.foundation.ir.arm2ir.ClassBoundIrBuilder
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.classFqName
 import org.jetbrains.kotlin.ir.types.classOrFail
 import org.jetbrains.kotlin.ir.types.isSubtypeOfClass
 import org.jetbrains.kotlin.ir.util.companionObject
 
 fun ClassBoundIrBuilder.adatCompanionOrNull(type : IrType) : IrExpression =
     if (type.isSubtypeOfClass(pluginContext.adatClass)) {
-        val companionClass = checkNotNull(type.classOrFail.owner.companionObject()) { "missing Adat companion; $type" }
+        val companionClass = checkNotNull(type.classOrFail.owner.companionObject()) { "missing Adat companion; ${type.classFqName}" }
         irGetObject(companionClass.symbol)
     } else {
         irNull()
