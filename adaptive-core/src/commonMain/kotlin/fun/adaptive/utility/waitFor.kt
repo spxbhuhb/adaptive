8da747f3979091193c52f0ea4f5f405bdbfdaaa8
Tóth István Zoltán <tiz@simplexion.hu>
2024-11-18 15:48:35 +0100

    [core] untilSuccess
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- adaptive-core/src/commonMain/kotlin/fun/adaptive/utility/waitFor.kt
+++ adaptive-core/src/commonMain/kotlin/fun/adaptive/utility/waitFor.kt
@@ -2,10 +2,15 @@ package `fun`.adaptive.utility
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 suspend fun waitFor(timeout: Duration, condition: () -> Boolean) {
     withTimeout(timeout) {
@@ -42,3 +47,79 @@ suspend inline fun <reified T> waitForNotNull(timeout: Duration, crossinline blo
         } while (value == null)
         value
     }
+
+suspend inline fun <reified T> untilNotNull(
+    catchTimeout: Boolean = true,
+    waitStrategy: WaitStrategy = WaitStrategy(),
+    crossinline block: suspend () -> T?,
+): T {
+
+    var value: T? = block()
+    if (value != null) return value
+
+    while (true) {
+        coroutineContext.ensureActive()
+
+        try {
+            value = block()
+            if (value != null) return value
+        } catch (ex: TimeoutCancellationException) {
+            if (! catchTimeout) throw ex
+        }
+
+        waitStrategy.wait()
+    }
+}
+
+suspend inline fun <reified T> untilSuccess(
+    waitStrategy: WaitStrategy = WaitStrategy(),
+    crossinline block: suspend () -> T,
+): T {
+
+    while (true) {
+        coroutineContext.ensureActive()
+
+        try {
+            return block()
+        } catch (_: TimeoutCancellationException) {
+            // timeout is fine, others we can actually throw
+        }
+
+        waitStrategy.wait()
+    }
+
+}
+
+suspend inline fun <reified T> untilNoTimeout(
+    waitStrategy: WaitStrategy = WaitStrategy(),
+    crossinline block: suspend () -> T,
+): T {
+
+    while (true) {
+        coroutineContext.ensureActive()
+
+        try {
+            return block()
+        } catch (_: TimeoutCancellationException) {
+            // timeout is fine, others we can actually throw
+        }
+
+        waitStrategy.wait()
+    }
+
+}
+
+class WaitStrategy(
+    val retryMultiplier: Int = 115,
+    initialDelay: Duration = 200.milliseconds,
+    maximumDelay: Duration = 5.seconds,
+) {
+
+    var retryDelay = initialDelay.inWholeMilliseconds
+    val maximumDelayMillis = maximumDelay.inWholeMilliseconds
+
+    suspend fun wait() {
+        delay(retryDelay)
+        if (retryDelay < maximumDelayMillis) retryDelay = (retryDelay * retryMultiplier) / 100
+    }
+}
