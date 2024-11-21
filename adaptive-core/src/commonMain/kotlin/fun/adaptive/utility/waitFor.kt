c5ddc51f8d42c2147c0ac7802c56a2f465bdecb4
Tóth István Zoltán <tiz@simplexion.hu>
2024-11-21 10:30:15 +0100

    [core] untilNotNull wait limit
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- adaptive-core/src/commonMain/kotlin/fun/adaptive/utility/waitFor.kt
+++ adaptive-core/src/commonMain/kotlin/fun/adaptive/utility/waitFor.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
+import kotlinx.datetime.Clock.System.now
 import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
@@ -47,79 +48,3 @@ suspend inline fun <reified T> waitForNotNull(timeout: Duration, crossinline blo
         } while (value == null)
         value
     }
-
-suspend inline fun <reified T> untilNotNull(
-    catchTimeout: Boolean = true,
-    waitStrategy: WaitStrategy = WaitStrategy(),
-    crossinline block: suspend () -> T?,
-): T {
-
-    var value: T? = block()
-    if (value != null) return value
-
-    while (true) {
-        coroutineContext.ensureActive()
-
-        try {
-            value = block()
-            if (value != null) return value
-        } catch (ex: TimeoutCancellationException) {
-            if (! catchTimeout) throw ex
-        }
-
-        waitStrategy.wait()
-    }
-}
-
-suspend inline fun <reified T> untilSuccess(
-    waitStrategy: WaitStrategy = WaitStrategy(),
-    crossinline block: suspend () -> T,
-): T {
-
-    while (true) {
-        coroutineContext.ensureActive()
-
-        try {
-            return block()
-        } catch (_: TimeoutCancellationException) {
-            // timeout is fine, others we can actually throw
-        }
-
-        waitStrategy.wait()
-    }
-
-}
-
-suspend inline fun <reified T> untilNoTimeout(
-    waitStrategy: WaitStrategy = WaitStrategy(),
-    crossinline block: suspend () -> T,
-): T {
-
-    while (true) {
-        coroutineContext.ensureActive()
-
-        try {
-            return block()
-        } catch (_: TimeoutCancellationException) {
-            // timeout is fine, others we can actually throw
-        }
-
-        waitStrategy.wait()
-    }
-
-}
-
-class WaitStrategy(
-    val retryMultiplier: Int = 115,
-    initialDelay: Duration = 200.milliseconds,
-    maximumDelay: Duration = 5.seconds,
-) {
-
-    var retryDelay = initialDelay.inWholeMilliseconds
-    val maximumDelayMillis = maximumDelay.inWholeMilliseconds
-
-    suspend fun wait() {
-        delay(retryDelay)
-        if (retryDelay < maximumDelayMillis) retryDelay = (retryDelay * retryMultiplier) / 100
-    }
-}
