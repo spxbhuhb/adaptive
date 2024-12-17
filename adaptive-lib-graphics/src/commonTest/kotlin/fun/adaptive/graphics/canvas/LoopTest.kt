98e2084e2a0349a5e56d2dc2ab6701000935b65e
Tóth István Zoltán <tiz@simplexion.hu>
2024-12-17 08:47:31 +0100

    #89 [plugin] test function lambda parameter resolution fails
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- adaptive-lib-graphics/src/commonTest/kotlin/fun/adaptive/graphics/canvas/LoopTest.kt
+++ adaptive-lib-graphics/src/commonTest/kotlin/fun/adaptive/graphics/canvas/LoopTest.kt
@@ -1,6 +1,5 @@
 package `fun`.adaptive.graphics.canvas
 
-import `fun`.adaptive.foundation.fragment
 import `fun`.adaptive.foundation.testing.test
 import `fun`.adaptive.graphics.canvas.api.canvas
 import `fun`.adaptive.graphics.canvas.api.fillText
@@ -11,7 +10,7 @@ class LoopTest {
     @Test
     fun basic() {
         test {
-            fragment().adapter.fragmentFactory += arrayOf(CanvasFragmentFactory)
+            it.fragmentFactory += arrayOf(CanvasFragmentFactory)
             canvas {
                 for (i in 0..10) {
                     fillText(i.toDouble(), i.toDouble(), i.toString())
