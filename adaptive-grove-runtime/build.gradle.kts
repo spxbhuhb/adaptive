44349b5f333916597a991acf32ec0855b83d5e76
Tóth István Zoltán <tiz@simplexion.hu>
2025-01-13 07:22:44 +0100

    [gradle] grove runtime publish fix
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- adaptive-grove-runtime/build.gradle.kts
+++ adaptive-grove-runtime/build.gradle.kts
@@ -1,10 +1,22 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 /*
  * Copyright © 2020-2024, Simplexion, Hungary and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.adaptive)
+    signing
+    alias(libs.plugins.gradleMavenPublish)
 }
+
+
+group = "fun.adaptive"
+version = libs.versions.adaptive.get()
+
+val baseName = "adaptive-grove-runtime"
+val pomName = "Adaptive Grove Runtime"
+val scmPath = "spxbhuhb/adaptive"
 
 // this is ugly but I don't use JS dependencies anyway, 
 // https://youtrack.jetbrains.com/issue/KT-50848/Kotlin-JS-inner-build-routines-are-using-vulnerable-NPM-dependencies-and-now-that-we-have-kotlin-js-store-github-audit-this
@@ -47,4 +59,45 @@ kotlin {
 
 tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.WARN
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+mavenPublishing {
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("fun.adaptive", baseName, version.toString())
+
+    pom {
+        description.set(project.name)
+        name.set(pomName)
+        url.set("https://adaptive.fun")
+        scm {
+            url.set("https://github.com/$scmPath")
+            connection.set("scm:git:git://github.com/$scmPath.git")
+            developerConnection.set("scm:git:ssh://git@github.com/$scmPath.git")
+        }
+        licenses {
+            license {
+                name.set("Apache 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("toth-istvan-zoltan")
+                name.set("Tóth István Zoltán")
+                url.set("https://github.com/toth-istvan-zoltan")
+                organization.set("Simplexion Kft.")
+                organizationUrl.set("https://www.simplexion.hu")
+            }
+        }
+    }
 }
