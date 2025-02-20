a422251a65f11d6db1ad7329d4b6db63d2e1063d
Tóth István Zoltán <tiz@simplexion.hu>
2025-02-20 07:47:16 +0100

    [ui] panel in sandbox with function references
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- sandbox/src/jsMain/kotlin/main.kt
+++ sandbox/src/jsMain/kotlin/main.kt
@@ -2,9 +2,16 @@
  * Copyright © 2020-2024, Simplexion, Hungary and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import `fun`.adaptive.adat.Adat
+import `fun`.adaptive.adat.api.update
+import `fun`.adaptive.adat.store.copyStore
 import `fun`.adaptive.backend.backend
 import `fun`.adaptive.foundation.Adaptive
+import `fun`.adaptive.foundation.AdaptiveFragment
+import `fun`.adaptive.foundation.fragment
+import `fun`.adaptive.foundation.instruction.AdaptiveInstruction
 import `fun`.adaptive.foundation.instruction.instructionsOf
+import `fun`.adaptive.foundation.instructions
 import `fun`.adaptive.graphics.canvas.CanvasFragmentFactory
 import `fun`.adaptive.graphics.svg.SvgFragmentFactory
 import `fun`.adaptive.grove.api.GroveRuntimeFragmentFactory
@@ -19,7 +26,7 @@ import `fun`.adaptive.ui.builtin.menu
 import `fun`.adaptive.ui.icon.icon
 import `fun`.adaptive.ui.instruction.DPixel
 import `fun`.adaptive.ui.instruction.dp
-import `fun`.adaptive.ui.instruction.layout.Width
+import `fun`.adaptive.ui.instruction.fr
 import `fun`.adaptive.ui.instruction.sp
 import `fun`.adaptive.ui.theme.backgrounds
 import `fun`.adaptive.ui.theme.colors
@@ -48,68 +55,113 @@ fun main() {
                 fontWeight = 300
             }
 
-            box {
-                maxSize
+            val workspace = copyStore { WorkSpace() }
 
-                panelIcons(true, panelConfig, panelTheme)
+            grid {
+                maxSize .. colTemplate(panelTheme.width, 1.fr, panelTheme.width)
+
+                panelIcons(left = true, panels, panelTheme, workspace)
+
+                grid {
+                    maxSize
+                    colTemplate(300.dp, 1.fr, 1.fr, 300.dp)
+                    rowTemplate(1.fr, 1.fr, 300.dp)
+
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.leftTop }) .. gridPos(1,1)
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.leftMiddle }) .. gridPos(2,1)
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.leftBottom }) .. gridPos(3,1)
+
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.center }) .. gridPos(1,2, colSpan = 2, rowSpan = 2)
+
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.rightTop }) .. gridPos(1,4)
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.rightMiddle }) .. gridPos(2,4)
+                    workspaceSlot(panels.firstOrNull { it.uuid == workspace.rightBottom }) .. gridPos(3,4)
+                }
+
+                panelIcons(left = false, panels, panelTheme, workspace)
             }
-
         }
     }
 }
 
+@Adaptive
+fun workspaceSlot(panel: Panel?, vararg instructions: AdaptiveInstruction): AdaptiveFragment {
+    box(instructions()) {
+        maxSize
+        if (panel != null) {
+            panelContent(panel.fragmentFun)
+        }
+    }
+    return fragment()
+}
 
-val panelConfig = PanelGroupConfiguration(
-    listOf(
-        Panel(
-            "Palette",
-            Graphics.menu,
-            PanelPosition.RightTop,
-            "⌘ P"
-        ),
-        Panel(
-            "Components",
-            Graphics.account_box,
-            PanelPosition.RightTop,
-            "⌘ P"
-        )
+val panels = listOf(
+    Panel(
+        UUID(),
+        "Palette",
+        Graphics.menu,
+        PanelPosition.LeftTop,
+        ::palette,
+        "⌘ P"
     ),
-    listOf(
-        Panel(
-            "Palette",
-            Graphics.menu,
-            PanelPosition.RightMiddle
-        ),
-        Panel(
-            "Components",
-            Graphics.account_box,
-            PanelPosition.RightMiddle,
-            "⌘ P"
-        )
+    Panel(
+        UUID(),
+        "Components",
+        Graphics.account_box,
+        PanelPosition.LeftTop,
+        ::emptyPanel,
+        "⌘ P"
     ),
-    listOf(
-        Panel(
-            "Palette",
-            Graphics.menu,
-            PanelPosition.RightBottom,
-            "⌘ P"
-        ),
-        Panel(
-            "Components",
-            Graphics.account_box,
-            PanelPosition.RightBottom,
-            "⌘ P"
-        )
+    Panel(
+        UUID(),
+        "Palette",
+        Graphics.menu,
+        PanelPosition.LeftMiddle,
+        ::emptyPanel
     ),
+    Panel(
+        UUID(),
+        "Components",
+        Graphics.account_box,
+        PanelPosition.LeftMiddle,
+        ::emptyPanel,
+        "⌘ P"
+    ),
+    Panel(
+        UUID(),
+        "Palette",
+        Graphics.menu,
+        PanelPosition.LeftBottom,
+        ::emptyPanel,
+        "⌘ P"
+    ),
+    Panel(
+        UUID(),
+        "Components",
+        Graphics.account_box,
+        PanelPosition.LeftBottom,
+        ::emptyPanel,
+        "⌘ P"
+    )
 )
 
-class PanelLayout(
-    rightTop : UUID<Panel>?,
-    rightMiddle : UUID<Panel>?,
-    rightBottom : UUID<Panel>?,
-    leftTop : UUID<Panel>?,
-    leftMiddle : UUID<Panel>?,
-    leftBottom : UUID<Panel>?
+@Adaptive
+fun panelContent(@Adaptive content: () -> AdaptiveFragment) {
+    box {
+        maxSize
+        content()
+    }
+}
+
+@Adat
+class WorkSpace(
+    val rightTop: UUID<Panel>? = null,
+    val rightMiddle: UUID<Panel>? = null,
+    val rightBottom: UUID<Panel>? = null,
+    val center: UUID<Panel>? = null,
+    val leftTop: UUID<Panel>? = null,
+    val leftMiddle: UUID<Panel>? = null,
+    val leftBottom: UUID<Panel>? = null
 )
 
 enum class PanelPosition {
@@ -122,25 +174,19 @@ enum class PanelPosition {
 }
 
 class Panel(
+    val uuid: UUID<Panel>,
     val name: String,
     val icon: GraphicsResourceSet,
     val position: PanelPosition,
-    val shortcut: String? = null,
-    val fragmentKey : String? = null
+    @Adaptive
+    val fragmentFun: () -> AdaptiveFragment,
+    val shortcut: String? = null
 )
-
-class PanelGroupConfiguration(
-    val top: List<Panel>,
-    val middle: List<Panel>,
-    val bottom: List<Panel>
-) {
-    val divider: Boolean = top.isNotEmpty() && middle.isNotEmpty()
-}
 
 val panelTheme = PanelTheme()
 
 class PanelTheme(
-    width: DPixel = 40.dp
+    val width: DPixel = 40.dp
 ) {
     val iconColumn = instructionsOf(
         maxHeight,
@@ -174,7 +220,8 @@ class PanelTheme(
         paddingVertical { 8.dp },
         backgrounds.reverse,
         cornerRadius { 4.dp },
-        gap { 8.dp }
+        gap { 8.dp },
+        zIndex { 100 }
     )
 
     val tooltipTextBase = instructionsOf(
@@ -190,42 +237,52 @@ class PanelTheme(
 
 @Adaptive
 fun panelIcons(
-    left : Boolean,
-    configuration: PanelGroupConfiguration,
-    theme: PanelTheme
+    left: Boolean,
+    panels: List<Panel>,
+    theme: PanelTheme,
+    workspace: WorkSpace
 ) {
+
+    val top = panels.filter(if (left) PanelPosition.LeftTop else PanelPosition.RightTop)
+    val middle = panels.filter(if (left) PanelPosition.LeftMiddle else PanelPosition.RightMiddle)
+    val bottom = panels.filter(if (left) PanelPosition.LeftBottom else PanelPosition.RightBottom)
 
     column {
         if (left) theme.leftIconColumn else theme.rightIconColumn
 
         column {
-            for (panel in configuration.top) {
-                panelIcon(panel, theme)
+            for (panel in top) {
+                panelIcon(panel, theme, workspace)
             }
-            if (configuration.divider) {
+            if (top.isNotEmpty() && middle.isNotEmpty()) {
                 box { theme.divider }
             }
-            for (panel in configuration.middle) {
-                panelIcon(panel, theme)
+            for (panel in middle) {
+                panelIcon(panel, theme, workspace)
             }
         }
 
         column {
-            for (panel in configuration.bottom) {
-                panelIcon(panel, theme)
+            for (panel in bottom) {
+                panelIcon(panel, theme, workspace)
             }
         }
     }
 
 }
 
+fun List<Panel>.filter(position: PanelPosition) =
+    filter { it.position == position }
+
 @Adaptive
 fun panelIcon(
     panel: Panel,
-    theme: PanelTheme
+    theme: PanelTheme,
+    workspace: WorkSpace
 ) {
     box {
         theme.panelIconContainer
+        onClick { workspace.switchTo(panel) }
 
         icon(panel.icon)
 
@@ -243,4 +300,33 @@ fun panelIcon(
             }
         }
     }
+}
+
+fun WorkSpace.switchTo(panel: Panel) {
+    when (panel.position) {
+        PanelPosition.RightTop -> update(::rightTop, panel.uuid)
+        PanelPosition.RightMiddle -> update(::rightMiddle, panel.uuid)
+        PanelPosition.RightBottom -> update(::rightBottom, panel.uuid)
+        PanelPosition.LeftTop -> update(::leftTop, panel.uuid)
+        PanelPosition.LeftMiddle -> update(::leftMiddle, panel.uuid)
+        PanelPosition.LeftBottom -> update(::leftBottom, panel.uuid)
+    }
+}
+
+@Adaptive
+fun emptyPanel(): AdaptiveFragment {
+    box {
+        padding { 16.dp }
+        text("todo")
+    }
+    return fragment()
+}
+
+@Adaptive
+fun palette() : AdaptiveFragment {
+    box {
+        padding { 16.dp }
+        text("palette")
+    }
+    return fragment()
 }
