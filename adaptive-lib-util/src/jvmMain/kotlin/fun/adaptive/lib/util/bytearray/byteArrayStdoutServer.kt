020449c09e95b6a141adf4a1c45b210b9f1eb2e8
Tóth István Zoltán <tiz@simplexion.hu>
2025-03-25 16:57:25 +0100

    [util] byte array print out server (UDP -> println)
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

---
+++ adaptive-lib-util/src/jvmMain/kotlin/fun/adaptive/lib/util/bytearray/byteArrayStdoutServer.kt
@@ -0,0 +1,16 @@
+package `fun`.adaptive.lib.util.bytearray
+
+import `fun`.adaptive.utility.clearedTestPath
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    val listener = ListenerByteArrayQueue(12675, clearedTestPath(), 1024L * 1024L, byteArrayOf(0x0A, 0x0A, 0x0A, 0x0A))
+    listener.start()
+
+    runBlocking {
+        while (true) {
+            val message = listener.queue.dequeue()
+            println(message.decodeToString())
+        }
+    }
+}
